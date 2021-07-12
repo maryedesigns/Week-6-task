4466a24c4751dc2161fff52225446791e81cb0da
maryedesigns <mary.erhieyovwe4christ@gmail.com>
2021-07-12 05:06:12 +0100

    Ecommerce Application

---
+++ products.js
@@ -0,0 +1,46 @@
+
+let Products = [
+    {
+        id: 1,
+        name: 'Plasma-Tv',
+        description: 'A-tv-set-high-definition-quality',
+        Image: 'Television-set.png',
+        price: 250000
+        
+    },
+
+    {
+        id: 2,
+        name: 'LG-laptops',
+        description: 'LG-quality-laptop',
+        Image: 'Laptops.png',
+        price: 180000
+        
+    },
+    
+    {
+        id: 3,
+        name: 'Samsung-galaxy-cameras',
+        description: 'High-resolution-HD-cameras',
+        Image: 'HD-cameras.png',
+        price: 880000
+    },
+  
+    {
+        id: 4,
+        name: 'Refrigirator',
+        description: 'Samsung-refrigerator',
+        Image: 'frige.png',
+        price: 1580000
+    },
+
+    {
+        id: 5,
+        name: 'Sneakers-boots',
+        description: 'Gucci-boots',
+        Image: 'boot.png',
+        price: 80000
+    },
+];
+
+module.exports = Products;
